01d5b83140ce813066999f19838ee15bffee2bdc
Badri Vasistha <badri.vasistha@gmail.com>
2019-09-24 14:11:21 -0700

    hello react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import Welcome from "./components /welcome";
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components /welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const welcome = () => {
+  return (
+    <div>
+      <p>Hello, React</p>
+    </div>
+  )
+}
+
+export default welcome
+
+
